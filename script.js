643619835d0fff7ddceb0d16313de351e9b138c0
AAA RENTAL LLP <aaarentalllp@Yashs-MacBook-Pro.local>
2025-05-27 14:29:01 +0530

    added confetti on completion

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ const addTask = ()=>{
 
 const toggleCompleteTask = (index) => {
     tasks[index].completed = !tasks[index].completed;
+    const completedTasks = tasks.filter(task => task.completed).length;
+    if(tasks.length === completedTasks){
+        blastConfetti();
+    }
     updateTaskList();
 }
 
@@ -76,3 +80,46 @@ addButton.addEventListener('click', (e) => {
 );
 
 updateTaskList();
+
+const blastConfetti = () => {
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+        Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+        })
+    );
+    }
+
+    fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+    });
+
+    fire(0.2, {
+    spread: 60,
+    });
+
+    fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+    });
+
+    fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+    });
+
+    fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+});
+
+}
